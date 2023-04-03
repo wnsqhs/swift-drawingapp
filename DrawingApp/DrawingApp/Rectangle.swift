91656ca11600a4994feef4efa9bf1f249af3d44b
wnsqhs <wnsqhs7804@gmail.com>
2023-04-03 14:55:40 +0900

    Factory 생성
    AbstractShapeFactory 구현 후 ShapeProperties 프로토콜 리턴
    RandomShapeFactory 클래스에서 AbstractShapeFactory 채택 후, 해당 프로토콜 타입 리턴
    공통된 필수 속성들을 구현해야 하는 ShapeProperties 프로토콜 구현
    실제로 구현할 Rectangle 클래스 구현 후 ShapeProperties 프로토콜 채택

--- DrawingApp/DrawingApp/Rectangle.swift
+++ DrawingApp/DrawingApp/Rectangle.swift
@@ -6,4 +6,46 @@
 //
 
 import Foundation
+protocol AbstractShapeFactory {
+    func creatRectangle() -> ShapeProperties
+}
 
+class RandomShapeFactory: AbstractShapeFactory {
+    func creatRectangle() -> ShapeProperties {
+        let id = ID().createID()
+        let size = Size(width: 150, height: 120)
+        let point = randomPosition()
+        let color = Color()
+        let alpha = Alpha.createRandomAlpha()
+        
+        return Rectangle(id: id, size: size, point: point, color: color, alpha: alpha)
+    }
+}
+
+protocol ShapeProperties: CustomStringConvertible {
+    var id: String { get }
+    var size: Size { get }
+    var point: Point { get }
+    var color: Color { get }
+    var alpha: Int { get }
+}
+
+class Rectangle: ShapeProperties {
+    var description: String {
+        return "\(id), \(size), \(point), \(color), \(alpha)"
+    }
+    
+    var id: String
+    var size: Size
+    var point: Point
+    var color: Color
+    var alpha: Int
+    
+    init(id: String, size: Size, point: Point, color: Color, alpha: Int) {
+        self.id = id
+        self.size = size
+        self.point = point
+        self.color = color
+        self.alpha = alpha
+    }
+}
