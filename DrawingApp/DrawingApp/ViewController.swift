3d0e1377fea5a593a218836fe8c4151a680f6f8d
wnsqhs <wnsqhs7804@gmail.com>
2023-04-03 14:59:12 +0900

    import OSLog
    OSLog를 이용하여, 로그 출력문 구현

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -6,10 +6,25 @@
 //
 
 import UIKit
+import OSLog
 
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        var log = Log()
+        log.printLog()
+    }
+}
+
+class Log {
+    let factory = RandomShapeFactory()
+    
+    func printLog() {
+        for i in 1...4 {
+            let rectangleView = factory.creatRectangle()
+            let logger = Logger(subsystem: "com.Aiden.DrawingApp", category: "ViewController")
+            logger.log("Rect \(i) \(rectangleView.description) ")
+        }
     }
 }
 
