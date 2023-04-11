cee667d6a9b0ffe39c38a27658eefddf44165918
wnsqhs <wnsqhs7804@gmail.com>
2023-04-12 00:08:29 +0900

    Plane 구조체 요구사항에 맞게 구현
    [Rectangle] 타입의 배열 생성
    해당 배열의 갯수를 반환하는 계산 프로퍼티 생성
    서브스크립트로 index를 넘기면 해당 index 사각형 리턴하는 서브스크립트 구현
    [Rectangle] 타입의 배열에 추가해주는 add 메소드 구현

--- DrawingApp/DrawingApp/Plane.swift
+++ DrawingApp/DrawingApp/Plane.swift
@@ -8,5 +8,24 @@
 import Foundation
 
 struct Plane {
-    
+    private(set) var newRectangleArray: [Rectangle]
+        
+        init() {
+            self.newRectangleArray = [Rectangle]()
+        }
+        
+        var rectangleCount: Int {
+            newRectangleArray.count
+        }
+        
+        subscript(index: Int) -> Rectangle? {
+            if index >= newRectangleArray.count {
+                return nil
+            }
+            return newRectangleArray[index]
+        }
+        
+        mutating func add(rectangle: Rectangle) {
+            self.newRectangleArray.append(rectangle)
+        }
 }
