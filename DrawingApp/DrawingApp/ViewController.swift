379f96008494158b2586fa1020666e508e0e2b3f
wnsqhs <wnsqhs7804@gmail.com>
2023-04-12 16:04:43 +0900

    createViewFromRectangle 메소드 생성
    파라미터로 받는 figure를 UIView로 변환 시켜주는 메소드 생성

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -9,22 +9,41 @@ import UIKit
 import OSLog
 
 class ViewController: UIViewController {
+    private var rectanglePlane = Plane()
+    private var randomShapeGenerator = RandomShapeGenerator()
+    lazy var shapeFactory = RandomShapeFactory(CommonConstituent: randomShapeGenerator)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let factory = RandomShapeFactory()
         printLog()
-        
-        func printLog() {
-            for i in 1...4 {
-                let rectangleView = factory.createRectangle()
-                let logger = Logger(subsystem: "com.Aiden.DrawingApp", category: "ViewController")
-                logger.log("Rect \(i) \(rectangleView.description) ")
-            }
+    }
+    
+    func printLog() {
+        for i in 1...4 {
+            let logRectangle = generateRandomRectangle()
+            let logger = Logger(subsystem: "com.Aiden.DrawingApp", category: "ViewController")
+            logger.log("Rect \(i) \(logRectangle.description) ")
         }
+    }
+    
+    func generateRandomRectangle() -> Rectangle {
+        return shapeFactory.createRectangle()
+    }
+    
+    func createViewFromRectangle(figure: Rectangle) -> UIView {
+        let viewIngredients = ViewTransformer(transformRectangle: figure)
+        let transformAlpha = viewIngredients.transformAlpha()
+        let transformColor = viewIngredients.transformColor()
+        let transformPoint = viewIngredients.transformPoint(point: figure.point)
+        
+        let rectangleView = UIView(frame: CGRect(x: transformPoint.x, y: transformPoint.y, width: figure.size.width, height: figure.size.height))
+        rectangleView.backgroundColor = UIColor(red: transformColor[0], green: transformColor[1], blue: transformColor[2], alpha: transformAlpha)
+        
+        return rectangleView
     }
 }
 
 class ScreenSize {
-    static let screenWidth = UIScreen.main.bounds.width
-    static let screenHeight = UIScreen.main.bounds.height
+    static let screenWidth = (UIScreen.main.bounds.width - 150)
+    static let screenHeight = (UIScreen.main.bounds.height - 120)
 }
