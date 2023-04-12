367182a8c750ada21fbe318fadcb821fbe9c9d0a
wnsqhs <wnsqhs7804@gmail.com>
2023-04-12 16:17:14 +0900

    IncludedRectangleInCoordinate 메소드 구현
    Tap할 때, 해당 뷰에 대한 좌표를 받아서 해당 좌표에 사각형이 존재한다면, true 아니면 false를 리턴하는 메소드 구현

--- DrawingApp/DrawingApp/Model/Plane.swift
+++ DrawingApp/DrawingApp/Model/Plane.swift
@@ -9,23 +9,36 @@ import Foundation
 
 struct Plane {
     private(set) var newRectangleArray: [Rectangle]
-        
-        init() {
-            self.newRectangleArray = [Rectangle]()
+    
+    init() {
+        self.newRectangleArray = [Rectangle]()
+    }
+    
+    var rectangleCount: Int {
+        newRectangleArray.count
+    }
+    
+    subscript(index: Int) -> Rectangle? {
+        if index >= newRectangleArray.count {
+            return nil
         }
-        
-        var rectangleCount: Int {
-            newRectangleArray.count
-        }
-        
-        subscript(index: Int) -> Rectangle? {
-            if index >= newRectangleArray.count {
-                return nil
+        return newRectangleArray[index]
+    }
+    
+    mutating func add(rectangle: Rectangle) {
+        self.newRectangleArray.append(rectangle)
+    }
+    
+    func IncludedRectangleInCoordinate(coordinate: CGPoint) -> Bool {
+        for index in 0..<newRectangleArray.count {
+            let selectedRectangle = newRectangleArray[index]
+            let xRange = selectedRectangle.point.positionX
+            let yRange = selectedRectangle.point.positionY
+            
+            if xRange <= coordinate.x && xRange + 150.0 >= coordinate.x &&  yRange <= coordinate.y && yRange + 120.0 >= coordinate.y {
+                return true
             }
-            return newRectangleArray[index]
         }
-        
-        mutating func add(rectangle: Rectangle) {
-            self.newRectangleArray.append(rectangle)
-        }
+        return false
+    }
 }
